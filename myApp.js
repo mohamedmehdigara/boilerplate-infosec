72ea8df7b6cfacf93852e35686dfd60817140c47
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-02-25 16:30:43 +0100

    Installed helmet and required it

--- myApp.js
+++ myApp.js
@@ -1,60 +1,15 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var helmet = require("helmet");
 
 module.exports = app;
-var api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+var api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
