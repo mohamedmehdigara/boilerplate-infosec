a556285bf74a8e4bd72912424bda4cda97ed367f
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-02-25 16:40:19 +0100

    Added helmet.hidePoweredBy()

--- myApp.js
+++ myApp.js
@@ -13,3 +13,4 @@ app.get("/", function (request, response) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+helmet.hidePoweredBy();
