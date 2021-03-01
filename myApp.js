8df20053b6769a5ca6ffad1ab4a36fa822c77e69
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-03-01 13:16:30 +0100

    added helmet.ieNoOpen()

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
 app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+
 
 module.exports = app;
 var api = require("./server.js");
