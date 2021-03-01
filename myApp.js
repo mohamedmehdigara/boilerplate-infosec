e760bb0570560cdc238d425233d357705a61d13d
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-03-01 13:14:07 +0100

    added helmet.noSniff()

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ var helmet = require("helmet");
 app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
 app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 module.exports = app;
 var api = require("./server.js");
